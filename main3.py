655d307e7ecee92d19459e606976c561d3fb6063
Mika Vainio <koti.mika.vainio@gmail.com>
2021-06-16 15:35:28 +0300

    Keskiviikko valmis

--- main3.py
+++ main3.py
@@ -29,27 +29,58 @@ class Ui(QtWidgets.QMainWindow):
         self.bigLcd = self.findChild(QtWidgets.QLCDNumber , 'frequencyLcd')
 
         # SIGNALS & SLOTS
-        self.playSound.clicked.connect(self.tone)
-        
+        # Play Sound button
+        self.playSound.clicked.connect(self.tone)  
+
+        # Dials
+        self.thousands.valueChanged.connect(self.updateLcd)
+        self.hundreds.valueChanged.connect(self.updateLcd)
+        self.tens.valueChanged.connect(self.updateLcd)
+        self.ones.valueChanged.connect(self.updateLcd)
+
+        # Menu actions
+        self.action1_kHz.triggered.connect(lambda: self.loadPreset(1, 0, 0, 0))
+        self.action100_Hz.triggered.connect(lambda: self.loadPreset(0, 1, 0, 0))
+        self.action666_Hz.triggered.connect(lambda: self.loadPreset(0, 6, 6, 6))
         # MAKE UI VISIBLE
         self.show()
 
     # METHODS
     # Play sound
     def tone(self):
-        sound.create_sound(500, 1)
+        if self.calculateFrequency() < 37:
+            sound.warn_sound()
+            alarmWindow = QtWidgets.QMessageBox()
+            alarmWindow.setText('Frequency shoud be 37 Hz at minimum')
+            alarmWindow.setWindowTitle('Frequency error')
+            alarmWindow.exec_()
+
+            # Set the minimum allowed value
+            self.tens.setValue(3)
+            self.ones.setValue(7)
+
+        else:        
+            sound.create_sound(self.calculateFrequency(), self.duration.value())
 
     # Calculate frequency
     def calculateFrequency(self):
-        pass
+        fthousands = self.thousands.value() * 1000
+        fhundreds = self.hundreds.value() * 100
+        ftens = self.tens.value() * 10
+        fones = self.ones.value()
+        frequency = fthousands + fhundreds + ftens + fones 
+        return frequency
 
     # Update the LCD
     def updateLcd(self):
-        pass
+        self.bigLcd.display(self.calculateFrequency())
 
     # Load preset
-    def loadPreset(self):
-        pass
+    def loadPreset(self, d1, d2, d3, d4):
+        self.thousands.setValue(d1)
+        self.hundreds.setValue(d2)
+        self.tens.setValue(d3)
+        self.ones.setValue(d4)
 
 # CREATE & RUN UI
 
